458b6eca4c4e8b0a8842e98e933a18646bb5e765
branden lee <branden.lee.hj@gmail.com>
2019-12-31 14:30:56 -0500

    hm will it work

--- my-app/src/GameBoardComponent.js
+++ my-app/src/GameBoardComponent.js
@@ -23,7 +23,7 @@ class Board extends React.Component {
             if(this.state.fPlayer) {
                 squares[i] = 'X';
             } else {
-                squares[i] = 'Z';
+                squares[i] = 'O';
             }
         this.setState({fPlayer: !this.state.fPlayer, squares: squares});
         }
